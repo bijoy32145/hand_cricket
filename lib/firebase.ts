b12b076a57e27014a8894b1b5c0996c0015a3434
bijoytechfriar <bijoy@techfriar.com>
2025-03-03 23:43:48 +0530

    #Changes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database"; // Import Realtime Database
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyAfR2ilodCVSSQbSz1HhYwREfXpbqLz6So",
   authDomain: "hand-cricket-b9e4c.firebaseapp.com",
+  databaseURL: " https://hand-cricket-b9e4c-default-rtdb.firebaseio.com/", // ✅ Add this line
   projectId: "hand-cricket-b9e4c",
-  storageBucket: "hand-cricket-b9e4c.appspot.com", // Fixed typo (firebasestorage.app -> appspot.com)
+  storageBucket: "hand-cricket-b9e4c.appspot.com",
   messagingSenderId: "879539357076",
   appId: "1:879539357076:web:57ebc670755d84277ffb81",
   measurementId: "G-MKBML2XEKW"
