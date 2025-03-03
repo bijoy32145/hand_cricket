8767b769bc318e538558c632bde7d74daaa0d468
bijoytechfriar <bijoy@techfriar.com>
2025-03-03 23:17:56 +0530

    #Changes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -18,4 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const database = getAnalytics(app);
+
+export { database };
